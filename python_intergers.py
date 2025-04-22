28d40e6c4dc01d76f5ea5d70779fc770629b8334
deepatirohit <rohit.deepati@hexanovamedia.tech>
2025-04-22 15:40:31 +0530

    integers (22-04-2025)

--- python_intergers.py
+++ python_intergers.py
@@ -1,48 +1,78 @@
-# Integers in python are whole numbers
+# # Integers in python are whole numbers
 
-a = 10
-print(a)
+# a = 10
+# print(a)
 
-# arthematic operators
+# # arthematic operators
 
-# addition
-num1 = 15
-num2 = 17
-sum = num1 + num2
-print(sum)
+# # addition
+# num1 = 15
+# num2 = 17
+# sum = num1 + num2
+# print(sum)
 
-# subtraction
-num1 = 14
-num2 = 28
-differnce = num2 - num1
-print(differnce)
+# # subtraction
+# num1 = 14
+# num2 = 28
+# differnce = num2 - num1
+# print(differnce)
 
-# mutliplication
-num1 = 29
-num2 = 10
-multplication = num1 * num2
-print(multplication)
+# # mutliplication
+# num1 = 29
+# num2 = 10
+# multplication = num1 * num2
+# print(multplication)
 
-# divison
-print(45/5)
+# # divison
+# print(45/5)
 
-# floor division
-print(47//4)
+# # floor division
+# print(47//4)
 
-# exponenet
-print(3 ** 3)
-# modulus (returns the remainder)
-print(55 % 6)
+# # exponenet
+# print(3 ** 3)
+# # modulus (returns the remainder)
+# print(55 % 6)
 
-# swaping numbers
-a = 10
-b = 20
+# # swaping numbers
+# a = 10
+# b = 20
 
-a, b = b, a
-print(a)
-print(b)
+# a, b = b, a
+# print(a)
+# print(b)
 
-# Square and Cube
-num = 5
-print(num ** 2)
-print(num ** 3)
+# # Square and Cube
+# num = 5
+# print(num ** 2)
+# print(num ** 3)
+
+# friends = 5
+
+# friends = friends + 1
+# friends += 1
+# friends =  friends - 2
+# friends -= 1
+# friends = friends  * 4
+# friends *= 4
+# friends = friends / 2
+# friends /= friends
+# print(friends)
+
+# built in math functions
+
+x = 3.14
+y = -4
+z = 10
+
+roundNumber = round(x)
+absoluteNumber = abs(y)
+powerNumber = pow(z, 3)
+maxNumber = max(x, y, z)
+minNumber = min(x, y, z)
+
+print(roundNumber)
+print(absoluteNumber)
+print(powerNumber)
+print(f"max number is {maxNumber}")
+print(f"Minimum number is {minNumber}")
