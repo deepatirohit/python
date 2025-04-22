0a6c82f40a02874fbfec13bd925a9237d2412b36
deepatirohit <rohit.deepati@hexanovamedia.tech>
2025-04-22 15:07:42 +0530

    integers (22-04-2025)

---
+++ python_intergers.py
@@ -0,0 +1,48 @@
+# Integers in python are whole numbers
+
+a = 10
+print(a)
+
+# arthematic operators
+
+# addition
+num1 = 15
+num2 = 17
+sum = num1 + num2
+print(sum)
+
+# subtraction
+num1 = 14
+num2 = 28
+differnce = num2 - num1
+print(differnce)
+
+# mutliplication
+num1 = 29
+num2 = 10
+multplication = num1 * num2
+print(multplication)
+
+# divison
+print(45/5)
+
+# floor division
+print(47//4)
+
+# exponenet
+print(3 ** 3)
+# modulus (returns the remainder)
+print(55 % 6)
+
+# swaping numbers
+a = 10
+b = 20
+
+a, b = b, a
+print(a)
+print(b)
+
+# Square and Cube
+num = 5
+print(num ** 2)
+print(num ** 3)
