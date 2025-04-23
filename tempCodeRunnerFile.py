6d8f5834cf5c9efad26f9a7b713a0ec89d2a2d36
deepatirohit <rohit.deepati@hexanovamedia.tech>
2025-04-23 15:21:51 +0530

    removed duplicate files

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-userInput =  input("Enter your name: ")
-# userAge = input("Enter your age: ")
-# # userAge = int(input("Enter your age: "))
-# userAge = int(userAge) + 1
-# print(f"Hello, I'm {userInput} and,  I'm {userAge} years old")
